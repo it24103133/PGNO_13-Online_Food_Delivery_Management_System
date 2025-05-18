31f55085e069fd3cb4ccc951139b606bd167e0be
it24103825 <it24103825@my.sliit.lk>
2025-05-18 11:41:15 +0530

    adding ReviewFileHandler class

---
+++ src/main/java/com/fooddelivery/online_food_delivery_management_system/util/ReviewFileHandler.java
@@ -0,0 +1,31 @@
+package com.fooddelivery.online_food_delivery_management_system.util;
+
+import com.fooddelivery.online_food_delivery_management_system.model.Review;
+
+import java.util.List;
+
+public class ReviewFileHandler {
+    public static void saveReview(Review review) {
+
+    }
+
+    public static List<Review> getReviewsByRestaurant(String restaurantId) {
+        return null;
+    }
+
+    public static List<Review> getReviewsByUser(String userId) {
+        return null;
+    }
+
+    public static Review getReviewById(String reviewId) {
+        return null;
+    }
+
+    public static void updateReview(Review updatedReview) {
+
+    }
+
+    public static void deleteReview(String reviewId) {
+
+    }
+}
