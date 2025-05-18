5eeee054b8afacc6951342c686cba42b8bd30f33
IT24103490 <it24103490@my.sliit.lk>
2025-05-19 01:16:06 +0530

    Update Appetizer.java file

--- src/main/java/com/fooddelivery/online_food_delivery_management_system/model/Appetizer.java
+++ src/main/java/com/fooddelivery/online_food_delivery_management_system/model/Appetizer.java
@@ -4,6 +4,13 @@ public class Appetizer {
     public class Appetizer extends MenuItem {
         public Appetizer(int id, String name, double price, String description, boolean available, int restaurantId) {
             super(id, name, price, description, "Appetizer", available, restaurantId);
+            this.id = id;
+            this.name =  name;
+            this.price = price;
+            this.description = description;
+            this.available = available;
+            this.restaurantId = restaurantId;
+
         }
 
         @Override
